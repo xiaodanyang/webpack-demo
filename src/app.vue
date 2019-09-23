8d43cd89db53a0f5bc56f8be26ca7f0d69ace65f
xdy <xiaodanyang@yuanben.org>
2019-09-23 11:26:51 +0800

    first pull

---
+++ src/app.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="example">
+        <div>{{text}}</div>
+        <mavon-editor v-model="value"></mavon-editor>
+    </div>
+</template>
+
+<script>
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+
+export default {
+    components: {
+        'mavon-editor': mavonEditor.mavonEditor
+    },
+    data() {
+        return {
+            text: 'abcde',
+            value: ''
+        }
+    }
+}
+</script>
+
+<style>
+#example {
+    color: #666;
+}
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+    mode: 'none',
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf|gif|png|jpe?g|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: '[name]-[hash:8]-yuanben.[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            title: '测试demo',
+            filename: 'index.html',
+            template: 'src/index.html',
+            chunksSortMode: 'none',
+            // inject: 'body',
+            // env: {
+            //     manifest: '/assets/manifest.json?v=0.3.9'
+            // },
+            // favicon: 'src/favicon.ico',
+            // hash: true
+        }),
+    ],
+}
