aec26570f76aa2b2003ac95b72ac6698a9adb8e0
xdy <164540455@qq.com>
2020-08-20 17:09:36 +0800

    update dependencies

--- src/app.vue
+++ src/app.vue
@@ -1,30 +1,23 @@
 <template>
-    <div id="example">
+    <div id="app">
         <div>{{text}}</div>
-        <mavon-editor v-model="value"></mavon-editor>
     </div>
 </template>
 
 <script>
-import mavonEditor from 'mavon-editor'
-import 'mavon-editor/dist/css/index.css'
 
 export default {
     components: {
-        'mavon-editor': mavonEditor.mavonEditor
+
     },
     data() {
         return {
-            text: 'abc',
-            value: ''
+            text: 'Hello word',
         }
     }
 }
 </script>
 
 <style>
-#example {
-    color: #666;
-}
 
 </style>
